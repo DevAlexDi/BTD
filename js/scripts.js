f8c27546d18f9da2b9e583f01397a53628829844
lexa <keitmark@mail.ru>
2017-12-11 21:20:33 +0300

    lala

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,47 @@ $(document).ready(function () {
         $('.slider-init').slick({
             slidesToShow: 1,
             slidesToScroll: 1,
-            arrows: false,
-            dots: false
+            arrows: true,
+            dots: false,
+            infinite:false
         });
     });
 
-
-
+     $('.flex-sights').slick({
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            arrows: true,
+            dots: false,
+            infinite:false
+        });
     
+    
+   
+    $('#sights-slider').on('hidden.bs.modal', function () {
+        $('.slider-init').slick('unslick');
+    });
+
+     if($(window).scrollTop() > 70){
+            $(".fixed-sights").addClass("transformed");
+        }
+        else{
+            $(".fixed-sights").removeClass("transformed");
+        }
+    
+
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 70){
+            $(".fixed-sights").addClass("transformed");
+        }
+        else{
+            $(".fixed-sights").removeClass("transformed");
+        }
+    });
+    $(".op-modal-sign-in").click(function(){
+         $("#login-modal").modal('show');
+    });
+    $(".op-modal-sign-up").click(function(){
+         $("#regist-modal").modal('show');
+    });
+  
 });
