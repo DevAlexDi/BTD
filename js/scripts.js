187322929d48d66672fa8104a51cedd639124a3b
lexa <keitmark@mail.ru>
2017-12-11 17:02:33 +0300

    trololo

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
-   
+
+    $(".one-sight").click(function () {
+        $("#sights-slider").modal("show");
+        $('.slider-init').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: false
+        });
+    });
+
+
+
+    
 });
